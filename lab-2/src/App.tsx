a77422aecaa6b1677224e6b8a6660b0dded298fa
Jyrko <uriydemoshenko@gmail.com>
2024-10-15 09:55:45 +0200

    sse

--- lab-2/src/App.tsx
+++ lab-2/src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import SearchBar from './components/SearchBar'
 import CharactersList from './components/CharactersList'
 import CharacterForm from './components/CharacterForm'
@@ -5,6 +6,20 @@ import CharacterForm from './components/CharacterForm'
 import { peopleData } from './data.js';
 
 function App() {
+  const [people, setPeople] = useState([]);
+  const [listening, setListening] = useState(false);
+
+  useEffect(() => {
+    if (!listening) {
+      const events = new EventSource('http://localhost:3000/events');
+      events.onmessage = (event) => {
+        const parsedData = JSON.parse(event.data);
+
+        setPeople((people) => people.concat(parsedData));
+      };
+    }
+    setListening(true);
+  }, [listening, people])
 
   const addCharacterHandler = (e) => {
     e.preventDefault();
@@ -19,7 +34,7 @@ function App() {
   return (
     <>
       <SearchBar searchHandler={searchHandler}/>
-      <CharactersList peopleData={peopleData}/>
+      <CharactersList peopleData={people}/>
       <CharacterForm addCharacterHandler={addCharacterHandler}/>
     </>
   )
