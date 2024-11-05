98638b62279c4ee948504d19227693c49f891f24
Jyrko <uriydemoshenko@gmail.com>
2024-11-05 10:26:36 +0100

    sync

--- lab-5/my-react-app/src/main.jsx
+++ lab-5/my-react-app/src/main.jsx
@@ -1,10 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import ErrorPage from "./error-page";
 import './index.css'
 import App from './App.jsx'
 
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    errorElement: <ErrorPage />
+  },
+]);
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
